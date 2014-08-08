4ab46710d090d236d9daa0aca5b138d096dd1e78
zhangdx <1032425782@qq.com>
2014-08-08 08:55:37 +0800

    添加路径规划的数据库接口

--- src/com/pkumap/db/PathPlanService.java
+++ src/com/pkumap/db/PathPlanService.java
@@ -1,9 +1,12 @@
 package com.pkumap.db;
 
+import java.util.ArrayList;
+
 import com.pkumap.bean.Road;
 import com.pkumap.bean.RoadNode;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class PathPlanService {
@@ -40,6 +43,69 @@ public class PathPlanService {
 		String[] args=new String[]{String.valueOf(road.getBegin()),String.valueOf(road.getEnd())
 				,String.valueOf(road.getDistance()),road.getType()};
 		db.execSQL(insert_sql, args);
+	}
+	/**
+	 * 根据Id获取RoadNode
+	 * @param id
+	 * @return
+	 */
+	public RoadNode getRoadNodeById(int id){
+		RoadNode roadNode=null;
+		String select_sql="select * from RoadNode where id=?";
+		String[] args=new String[]{String.valueOf(id)};
+		Cursor cursor=db.rawQuery(select_sql, args);
+		if(cursor.moveToNext()){
+			roadNode=new RoadNode();
+			roadNode.setId(cursor.getInt(cursor.getColumnIndex("id")));
+			roadNode.setX(cursor.getFloat(cursor.getColumnIndex("x")));
+			roadNode.setY(cursor.getFloat(cursor.getColumnIndex("y")));
+			roadNode.setType(cursor.getString(cursor.getColumnIndex("type")));
+		}
+		cursor.close();
+		return roadNode;
+	}
+	/**
+	 * 根据地图类型获取路径规划所有点的信息
+	 */
+	public ArrayList<RoadNode> getAllPointInfoByType(String type){
+		ArrayList<RoadNode> roadNodes=new ArrayList<RoadNode>();
+		String select_sql="select * from RoadNode where type=?";
+		String[] args=new String[]{type};
+		Cursor cursor=db.rawQuery(select_sql, args);
+		roadNodes.add(new RoadNode(0,0,0,type));
+		while(cursor.moveToNext()){
+			RoadNode roadNode=new RoadNode();
+			roadNode.setId(cursor.getInt(cursor.getColumnIndex("id")));
+			roadNode.setX(cursor.getFloat(cursor.getColumnIndex("x")));
+			roadNode.setY(cursor.getFloat(cursor.getColumnIndex("y")));
+			roadNode.setType(type);
+			
+			roadNodes.add(roadNode);
+		}
+		cursor.close();
+		return roadNodes;
+	}
+	/**
+	 * 根据地图类型获取路径规划所有路的信息
+	 * @param type
+	 * @return
+	 */
+	public ArrayList<Road> getAllRoadInfoByType(String type){
+		ArrayList<Road> roads=new ArrayList<Road>();
+		String select_sql="select * from Road where type=?";
+		String[] args=new String[]{type};
+		Cursor cursor=db.rawQuery(select_sql, args);
+		while(cursor.moveToNext()){
+			Road road=new Road();
+			road.setBegin(cursor.getInt(cursor.getColumnIndex("begin")));
+			road.setEnd(cursor.getInt(cursor.getColumnIndex("end")));
+			road.setDistance(cursor.getFloat(cursor.getColumnIndex("distance")));
+			road.setType(type);
+			
+			roads.add(road);
+		}
+		cursor.close();
+		return roads;
 	}
 	/**
 	 * 创建RoadNode的表
