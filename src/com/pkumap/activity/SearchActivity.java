53cd15864c7f5d5be93cbdc577940669a279558e
zhangdx <1032425782@qq.com>
2014-08-14 18:03:38 +0800

    添加对三维中POI数据的检索

--- src/com/pkumap/activity/SearchActivity.java
+++ src/com/pkumap/activity/SearchActivity.java
@@ -3,6 +3,7 @@ package com.pkumap.activity;
 import java.util.ArrayList;
 
 import com.pkumap.eventlistener.SearchPoiOnClickListener;
+import com.pkumap.util.BuildingManager;
 import com.pkumap.util.PoiManager;
 import com.zdx.pkumap.R;
 
@@ -17,7 +18,9 @@ import android.widget.EditText;
 public class SearchActivity extends Activity {
 	private AutoCompleteTextView search_edit=null;
 	private Button search_btn=null;
-	private PoiManager poiManager;
+	public PoiManager poiManager;
+	public BuildingManager buildingManager;
+	public String search_map_type="2dmap";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -25,18 +28,29 @@ public class SearchActivity extends Activity {
 		setContentView(R.layout.search_main);
 		
 		poiManager=new PoiManager(this.getApplicationContext());
+		buildingManager=new BuildingManager(this.getApplicationContext());
 		
-		search_edit=(AutoCompleteTextView) findViewById(R.id.poi_search_txt);
-		ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line,poiManager.getPoiNameFromDB());
-		search_edit.setAdapter(arrayAdapter);
-		search_edit.setThreshold(1);
-		
-		search_btn=(Button) findViewById(R.id.poi_search_btn);
-		search_btn.setOnClickListener(new SearchPoiOnClickListener(SearchActivity.this,poiManager));
 		Intent intent=getIntent();
 		Bundle bundle=intent.getExtras();
 		String str=bundle.getString("poi_name_search");
+		this.search_map_type=bundle.getString("map_type");
+		ArrayList<String> name=new ArrayList<String>();
+		if("3dmap".equals(search_map_type)){
+			name=buildingManager.getAllBuildingNames();
+		}else{
+			name=poiManager.getPoiNameFromDB();
+		}
+		
+		search_edit=(AutoCompleteTextView) findViewById(R.id.poi_search_txt);
+		ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line,name);
+		search_edit.setAdapter(arrayAdapter);
+		search_edit.setThreshold(1);
+		
+		SearchPoiOnClickListener searchPoiOnClickListener=new SearchPoiOnClickListener(this);
+		
+		search_btn=(Button) findViewById(R.id.poi_search_btn);
+		search_btn.setOnClickListener(searchPoiOnClickListener);
 		search_edit.setText(str);
 	}
 	@Override
@@ -44,6 +58,7 @@ public class SearchActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		poiManager.close();
+		buildingManager.close();
 	}
 	
 	
