f4bc044e80d1f3cfe1ae373c6c730d727fc79f92
zhangdx <1032425782@qq.com>
2014-08-14 18:04:56 +0800

    添加到POI详细信息中的到这去的点击事件

--- src/com/pkumap/activity/PoiDetailFragment.java
+++ src/com/pkumap/activity/PoiDetailFragment.java
@@ -1,20 +1,29 @@
 package com.pkumap.activity;
 
 import com.pkumap.bean.Poi;
+import com.pkumap.eventlistener.MapOnClickListener;
+import com.pkumap.eventlistener.PathPlanOnClickListener;
 import com.zdx.pkumap.R;
 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PoiDetailFragment extends Fragment {
 	private TextView poi_name_txt=null;
 	private TextView poi_addr_txt=null;
 	private TextView poi_desc_txt=null;
+	private RadioButton poi_go_btn=null;
 	private Poi poi;
+	public PoiDetailFragment(){
+		poi=new Poi();
+	}
 	public PoiDetailFragment(Poi poi){
 		this.poi=poi;
 	}
@@ -38,13 +47,15 @@ public class PoiDetailFragment extends Fragment {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		poi_name_txt=(TextView)getView().findViewById(R.id.poi_detail_name);
-		 poi_name_txt.setText(poi.getName());
-		    
-		 poi_addr_txt=(TextView)getView().findViewById(R.id.poi_detail_addr);
-		 poi_addr_txt.setText(poi.getAddr());
-		    
-		 poi_desc_txt=(TextView)getView().findViewById(R.id.poi_detail_desc);
-		 poi_desc_txt.setText(poi.getDesc());
+		poi_addr_txt=(TextView)getView().findViewById(R.id.poi_detail_addr);
+		poi_desc_txt=(TextView)getView().findViewById(R.id.poi_detail_desc);
+		poi_go_btn=(RadioButton) getView().findViewById(R.id.poi_go);
+		if(null!=poi){
+			poi_name_txt.setText(poi.getName());
+			 poi_addr_txt.setText(poi.getAddr());
+			 poi_desc_txt.setText(poi.getDesc());
+		}
+		poi_go_btn.setOnClickListener(new MapOnClickListener((MapActivity)this.getActivity()));
 	}
 	
 }
