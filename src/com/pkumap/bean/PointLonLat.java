979f89047210d1fb737fdb167ce5c4a350c12476
zhangdx <1032425782@qq.com>
2014-08-16 18:41:22 +0800

    对PointLonLat进行序列化

--- src/com/pkumap/bean/PointLonLat.java
+++ src/com/pkumap/bean/PointLonLat.java
@@ -1,6 +1,9 @@
 package com.pkumap.bean;
 
-public class PointLonLat {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class PointLonLat implements Parcelable {
 	private double x;
 	private double y;
 	
@@ -22,6 +25,32 @@ public class PointLonLat {
 	}
 	public void setY(double y) {
 		this.y = y;
+	}
+	/** 
+     * 序列化实体类 
+     */  
+    public static final Parcelable.Creator<PointLonLat> CREATOR = new Creator<PointLonLat>() {  
+        public PointLonLat createFromParcel(Parcel source) {  
+        	PointLonLat gpsLonLat = new PointLonLat();
+        	gpsLonLat.x=source.readDouble();
+        	gpsLonLat.y=source.readDouble();
+            return gpsLonLat;  
+        }  
+  
+        public PointLonLat[] newArray(int size) {  
+            return new PointLonLat[size];  
+        }  
+    };  
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+		dest.writeDouble(x);
+		dest.writeDouble(y);
 	}
 	
 }
