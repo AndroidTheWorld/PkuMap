e41d7846239bf2ced2ed2ba6e300378149104906
zhangdx <1032425782@qq.com>
2014-08-16 18:42:28 +0800

    给RoadNode表添加Gps字段并更新数据，同时可以根据范围获取PointId

--- src/com/pkumap/db/PathPlanService.java
+++ src/com/pkumap/db/PathPlanService.java
@@ -2,6 +2,7 @@ package com.pkumap.db;
 
 import java.util.ArrayList;
 
+import com.pkumap.bean.PointLonLat;
 import com.pkumap.bean.Road;
 import com.pkumap.bean.RoadNode;
 
@@ -63,6 +64,46 @@ public class PathPlanService {
 		}
 		cursor.close();
 		return roadNode;
+	}
+	/**
+	 * 更新GPS坐标
+	 * @param poiId
+	 * @param gpsPoint
+	 */
+	public void updateGpsXYInRoadNode(int nodeId,PointLonLat gpsPoint,String type){
+		double gps_x=gpsPoint.getX();
+		double gps_y=gpsPoint.getY();
+		String update_sql="update RoadNode set gps_x=?,gps_y=? where id=? and type=?";
+		String[] args=new String[]{String.valueOf(gps_x),String.valueOf(gps_y),String.valueOf(nodeId),type};
+		
+		db.execSQL(update_sql, args);
+	}
+	/**
+	 * 更新表的结构，添加Gps的坐标信息
+	 */
+	public void updateRoadNodeTableAddGps(){
+		String update_sql="ALTER TABLE RoadNode ADD COLUMN gps_y REAL";
+		db.execSQL(update_sql);
+	}
+	/**
+	 * 根据Gps的位置获取PointId
+	 * @return
+	 */
+	public int getPointIdFromGps(PointLonLat pointLonLat,double dx,double dy){
+		int pointId=-1;
+		
+		double gps_x=pointLonLat.getX();
+		double gps_y=pointLonLat.getY();
+		
+		String select_sql="select id from RoadNode where (gps_x between "+(gps_x-dx)+" and "+(gps_x+dx)+") and (gps_y between "
+		+(gps_y-dy)+" and "+(gps_y+dy)+")";
+		Cursor cursor=db.rawQuery(select_sql, null);
+		//只取第一条数据之后再做考虑
+		if(cursor.moveToNext()){
+			pointId=cursor.getInt(cursor.getColumnIndex("id"));
+		}
+		cursor.close();
+		return pointId;
 	}
 	/**
 	 * 根据地图类型获取路径规划所有点的信息
