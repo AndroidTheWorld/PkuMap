3d3b90b29e48e84c7556e6aeb4660eff5c87872b
zhangdx <1032425782@qq.com>
2014-08-25 10:08:19 +0800

    路径导航的消息处理类，与UI线程进行交互

---
+++ src/com/pkumap/util/NaviGpsHandler.java
@@ -0,0 +1,49 @@
+package com.pkumap.util;
+
+import com.pkumap.activity.MapActivity;
+import com.pkumap.bean.Point;
+import com.pkumap.bean.RoadNode;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+public class NaviGpsHandler extends Handler {
+
+	private MapActivity context;
+	public NaviGpsHandler(MapActivity context){
+		this.context=context;
+	}
+	@Override
+	public void handleMessage(Message msg) {
+		switch (msg.what) {
+		case 101:
+			Bundle bundle=msg.getData();
+			String naviSpeakTxt=bundle.getString("naviSpeakTxt");
+			RoadNode curLocRoadNode=bundle.getParcelable("curLoc");
+			
+			context.audioManager.mSpeech.speak(naviSpeakTxt,TextToSpeech.QUEUE_ADD, null);
+			if(curLocRoadNode!=null){
+				context.mapView.curLocation=new Point(curLocRoadNode.getX(),curLocRoadNode.getY());
+				context.mapView.currentStatus=context.mapView.STATUS_INIT;
+				context.mapView.invalidate();
+			}
+			
+	/*		if(MapActivity.gpsLonLat!=null){
+				Toast.makeText(context, "当前坐标：X:"+MapActivity.gpsLonLat.getX()+",Y:"
+							+MapActivity.gpsLonLat.getY(), Toast.LENGTH_SHORT).show();
+			}else{
+				context.audioManager.mSpeech.speak("GPS当前不可用",TextToSpeech.QUEUE_FLUSH, null);
+			}*/
+			
+			break;
+			default:
+				break;
+		}
+		super.handleMessage(msg);
+	}
+	
+}
