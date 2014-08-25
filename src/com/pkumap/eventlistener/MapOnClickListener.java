ce004e258b7ce6d8a6d6a025c2895224eb9485b4
zhangdx <1032425782@qq.com>
2014-08-25 10:04:55 +0800

    增加路径导航的功能，以及一些边界值的判断

--- src/com/pkumap/eventlistener/MapOnClickListener.java
+++ src/com/pkumap/eventlistener/MapOnClickListener.java
@@ -7,29 +7,29 @@ import com.pkumap.activity.MapView;
 import com.pkumap.activity.PathPlanActivity;
 import com.pkumap.activity.SearchActivity;
 import com.pkumap.bean.Poi;
+import com.pkumap.bean.Point;
 import com.pkumap.bean.PointLonLat;
 import com.pkumap.bean.RoadNode;
-import com.pkumap.util.PathPlanManager;
+import com.pkumap.manager.NaviManager;
+import com.pkumap.manager.TimerManager;
+import com.pkumap.util.NaviGpsHandler;
 import com.pkumap.util.RoadPlan;
 import com.zdx.pkumap.R;
 
-import android.R.anim;
-import android.app.Activity;
-import android.app.Fragment;
+
+
+
 import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v4.app.FragmentActivity;
+import android.speech.tts.TextToSpeech;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Toast;
 
 public class MapOnClickListener implements OnClickListener {
@@ -41,19 +41,28 @@ public class MapOnClickListener implements OnClickListener {
 	private View layerView;
 	private RadioButton radio_layer_three=null;
 	private RadioButton radio_layer_two=null;
-
 	private PopupWindow layers_window;
+	private NaviGpsHandler naviGpsHandler;
+	private NaviManager naviManager;
 	public MapOnClickListener(MapActivity target_Activity) {
 		this.event_Activity=target_Activity;
-		poi_edit_txt=(EditText) event_Activity.findViewById(R.id.poi_edit_search);
-		layers_img_view=(ImageView) event_Activity.findViewById(R.id.layers);
-		mapView=(MapView) event_Activity.findViewById(R.id.mapView);
-		
+		poi_edit_txt=target_Activity.edit_search;
+		layers_img_view=target_Activity.layers;
+		mapView=target_Activity.mapView;
+	
 		layerView=LayoutInflater.from(event_Activity).inflate(R.layout.layer_main,null);
 		radio_layer_three=(RadioButton)layerView.findViewById(R.id.three_layer);
 	    radio_layer_two=(RadioButton) layerView.findViewById(R.id.two_layer);
 	    radio_layer_three.setOnClickListener(this);
         radio_layer_two.setOnClickListener(this);
+        
+        event_Activity.roadPlan=new RoadPlan(mapView);
+        
+        naviGpsHandler=new NaviGpsHandler(event_Activity);
+
+        event_Activity.timerManager=new TimerManager(naviGpsHandler);
+        
+        naviManager=NaviManager.getInstance();
 	}
 	@Override
 	public void onClick(View v) {
@@ -88,30 +97,109 @@ public class MapOnClickListener implements OnClickListener {
 			goToPoi(mapView.poi);
 			break;
 		case R.id.nearby:
-			Toast.makeText(event_Activity, "附近", Toast.LENGTH_SHORT).show();
+//			Toast.makeText(event_Activity, "附近", Toast.LENGTH_SHORT).show();
+			CancelTimer();
 			break;
 		case R.id.pathplan:
-			confirmStartEndLocation();
+			confirmStartEndLocation(MapActivity.RESULT_PATHPATH);
+			break;
+		case R.id.navigation:
+			confirmStartEndLocation(MapActivity.RESULT_NAVI);
+			break;
+		case R.id.tool:
+			TestAudio();
+//			TestTimer();
 			break;
 		}
+	}
+	/**
+	 * 测试语音功能
+	 */
+	public void TestAudio(){
+		//测试：从北大计算中心----->百年纪念讲堂  A-->B-->C-->D-->E-->F
+//		PointLonLat srcLonLat=new PointLonLat(116.306427819824,39.9891760681152);   A
+//		PointLonLat destLonLat=new PointLonLat(116.306443078613,39.988985333252);   B
+		
+//		PointLonLat srcLonLat=new PointLonLat(116.306443078613,39.988985333252);   B
+//		PointLonLat destLonLat=new PointLonLat(116.305161340332,39.9889891479492); C
+		
+//		PointLonLat srcLonLat=new PointLonLat(116.305161340332,39.9889891479492);  C
+//		PointLonLat destLonLat=new PointLonLat(116.305176599121,39.9886114929199); D
+		
+//		PointLonLat srcLonLat=new PointLonLat(116.305176599121,39.9886114929199);  D
+//		PointLonLat destLonLat=new PointLonLat(116.304932458496,39.9886191223145); E
+		
+//		PointLonLat srcLonLat=new PointLonLat(116.304932458496,39.9886191223145);  E
+//		PointLonLat destLonLat=new PointLonLat(116.304726464844,39.9884627197266); F
+		
+//		PointLonLat srcLonLat=new PointLonLat(116.3017425,39.97200388888889);
+		PointLonLat srcLonLat=event_Activity.gpsLonLat;//有时候因为GPS受天气影响，会有一定的偏差
+		PointLonLat destLonLat=new PointLonLat(116.30178083333334,39.97201527777778);
+		
+		String pathNavi=naviManager.GetOrientAndDistance(srcLonLat, destLonLat);
+		event_Activity.audioManager.mSpeech.speak(pathNavi,TextToSpeech.QUEUE_FLUSH, null);
+	}
+	/**
+	 * 测试定时器
+	 */
+	public void TestTimer(){
+		event_Activity.timerManager.startTimer();
+	}
+	/**
+	 * 取消定时器
+	 */
+	public void CancelTimer(){
+		Toast.makeText(event_Activity, "关闭语音",Toast.LENGTH_SHORT ).show();
+		event_Activity.timerManager.stopTimer();
+//		naviGpsHandler.removeCallbacks(null);
 	}
 	/**
 	 * 将地图移动到以当前位置为中心（由Gps获取）
 	 */
 	private void  getCurLocationInMap(){
+//		MapActivity.gpsLonLat=new PointLonLat(116.305530,39.992543);
 		if(MapActivity.gpsLonLat!=null){
-			Toast.makeText(event_Activity,"当前GPS坐标："+MapActivity.gpsLonLat.getX()+","+MapActivity.gpsLonLat.getY(), Toast.LENGTH_SHORT).show();
-			poi_edit_txt.setText("当前GPS坐标："+MapActivity.gpsLonLat.getX()+","+MapActivity.gpsLonLat.getY());
+			if(curLocIsInPku(MapActivity.gpsLonLat)){
+//				Toast.makeText(event_Activity,"当前GPS坐标："+MapActivity.gpsLonLat.getX()+","+MapActivity.gpsLonLat.getY(), Toast.LENGTH_SHORT).show();
+				poi_edit_txt.setText("当前GPS坐标："+MapActivity.gpsLonLat.getX()+","+MapActivity.gpsLonLat.getY());
+				RoadNode near_node=event_Activity.roadPlan.getNearRoadNodeFromCurGps(MapActivity.gpsLonLat);
+				if(null==near_node){
+					Toast.makeText(event_Activity, "当前位置无法获取路口点信息，请到距路近的地方重试",Toast.LENGTH_SHORT ).show();
+					return;
+				}
+				Point point=new Point(near_node.getX(),near_node.getY());
+				Point screenPoint=event_Activity.convertCoordinate.getScreenPointFromLonLat(point, mapView);
+				mapView.curLocation=point;
+				mapView.moveDX=mapView.ScreenWidth/2-screenPoint.getX();
+				mapView.moveDY=mapView.ScreenHeight/2-screenPoint.getY();
+				mapView.currentStatus=mapView.STATUS_MOVE;
+				mapView.invalidate();
+			}else{
+				Toast.makeText(event_Activity, "当前位置不在北大",Toast.LENGTH_SHORT ).show();
+				poi_edit_txt.setText("当前GPS坐标："+MapActivity.gpsLonLat.getX()+","+MapActivity.gpsLonLat.getY());
+			}
 		}else{
 			Toast.makeText(event_Activity, "当前GPS不可用", Toast.LENGTH_SHORT).show();
 		}
+	}
+	/**
+	 * 当前的位置是否在北大内部
+	 */
+	private boolean curLocIsInPku(PointLonLat curLonLat){
+		//北大南门（39.985348,116.305478）,北大北门（39.997887,116.303368）
+		//北大西门 （39.993367,116.298173）,北大东门（39.990860,116.309416）
+		double x=curLonLat.getX();
+		double y=curLonLat.getY();
+		if(x>116.298173&&x<116.309416&&y>39.985348&&y<39.997887){
+			return true;
+		}
+		return false;
 	}
 	/**
 	 * 从当前位置到POI的位置
 	 * @param poi
 	 */
 	private void goToPoi(Poi poi){
-		RoadPlan roadPlan=new RoadPlan(mapView);
 //		ArrayList<RoadNode> roadNodes=roadPlan.getRoadNodes("博雅塔", poi.getName(),mapView.map_type);
 //		PointLonLat gpsLonLat=new PointLonLat(116.298708,39.993326);//北大西门
 //		PointLonLat gpsLonLat=new PointLonLat(116.306478,39.989591);//理科一号楼(门前)
@@ -125,12 +213,12 @@ public class MapOnClickListener implements OnClickListener {
 		}
 		PointLonLat gpsLonLat=new PointLonLat(MapActivity.gpsLonLat.getX(),MapActivity.gpsLonLat.getY()); //Gps数据
 		
-		int curPointId=roadPlan.getPointIdFromCurGps(gpsLonLat);
+		int curPointId=event_Activity.roadPlan.getPointIdFromCurGps(gpsLonLat);
 		if(curPointId==-1){
 			Toast.makeText(event_Activity, "当前路径不可达", Toast.LENGTH_SHORT).show();
 			return;
 		}
-		ArrayList<RoadNode> roadNodes=roadPlan.getRoadNodeInPath(curPointId, poi.getPointId(), mapView.map_type);
+		ArrayList<RoadNode> roadNodes=event_Activity.roadPlan.getRoadNodeInPath(curPointId, poi.getPointId(), mapView.map_type);
 //		Toast.makeText(event_Activity,roadNodes.size(), Toast.LENGTH_SHORT).show();
 		event_Activity.showPathInMap(roadNodes);
 	}
@@ -214,11 +302,23 @@ public class MapOnClickListener implements OnClickListener {
 	/**
 	 * 确定路径规划的起点和终点
 	 */
-	private void confirmStartEndLocation(){
+	private void confirmStartEndLocation(int resultCode){
+		if(resultCode==MapActivity.RESULT_NAVI){
+			if(MapActivity.gpsLonLat!=null){
+				if(!curLocIsInPku(MapActivity.gpsLonLat)){
+					Toast.makeText(event_Activity, "当前位置不在北大,无法导航！",Toast.LENGTH_SHORT ).show();
+					return;
+				}
+			}else{
+				Toast.makeText(event_Activity, "当前无法获取GPS信息", Toast.LENGTH_SHORT).show();
+				return;
+			}
+		}
 		Intent intent=new Intent();
 		intent.setClass(event_Activity, PathPlanActivity.class);
 		intent.putExtra("map_type",mapView.map_type);
-		event_Activity.startActivityForResult(intent, 0);
+		intent.putExtra("type", resultCode);
+		event_Activity.startActivityForResult(intent, resultCode);
 	}
 	/**
 	 * 跳转SearchActivity,进行POI的搜索
