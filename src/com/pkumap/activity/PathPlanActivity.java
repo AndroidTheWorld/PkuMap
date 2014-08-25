47aae4fb80f22bd05448e3f69c036fbcea196a8f
zhangdx <1032425782@qq.com>
2014-08-25 09:59:10 +0800

    导航的功能实现

--- src/com/pkumap/activity/PathPlanActivity.java
+++ src/com/pkumap/activity/PathPlanActivity.java
@@ -3,9 +3,9 @@ package com.pkumap.activity;
 import java.util.ArrayList;
 
 import com.pkumap.eventlistener.PathPlanOnClickListener;
-import com.pkumap.util.BuildingManager;
-import com.pkumap.util.PathPlanManager;
-import com.pkumap.util.PoiManager;
+import com.pkumap.manager.BuildingManager;
+import com.pkumap.manager.PathPlanManager;
+import com.pkumap.manager.PoiManager;
 import com.zdx.pkumap.R;
 
 import android.app.Activity;
@@ -21,29 +21,26 @@ public class PathPlanActivity extends Activity {
 	private Button btn_import=null;
 	private AutoCompleteTextView path_start_txt=null;
 	private AutoCompleteTextView path_end_txt=null;
-	public final static int RESULT_PATHPLAN=3;
 	public PathPlanManager pathPlanManager;
 	public PoiManager poiManager;
 	public BuildingManager buildingManager;
 	public String map_type="2dmap";
+	public int typeCode=-1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pathplan_main);
+	
+		Intent intent=getIntent();
+		map_type=intent.getStringExtra("map_type");
+		typeCode=intent.getIntExtra("type", MapActivity.RESULT_PATHPATH);
 		
 		pathPlanManager=new PathPlanManager(this.getApplicationContext());
 		poiManager=new PoiManager(this.getApplicationContext());
 		buildingManager=new BuildingManager(this.getApplicationContext());
 		PathPlanOnClickListener pathPlanOnClickListener=new PathPlanOnClickListener(this);
 		
-		btn_return=(Button) findViewById(R.id.path_return);
-		btn_return.setOnClickListener(pathPlanOnClickListener);
-		
-		btn_search=(Button) findViewById(R.id.path_search);
-		btn_search.setOnClickListener(pathPlanOnClickListener);
-		Intent intent=getIntent();
-		map_type=intent.getStringExtra("map_type");
 		
 		ArrayList<String> name=new ArrayList<String>();
 		if("3dmap".equals(map_type)){
@@ -54,13 +51,26 @@ public class PathPlanActivity extends Activity {
 		path_start_txt=(AutoCompleteTextView) findViewById(R.id.path_start);
 		ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,
 				android.R.layout.simple_dropdown_item_1line,name);
-		path_start_txt.setAdapter(arrayAdapter);
-		path_start_txt.setThreshold(1);
+		if(MapActivity.RESULT_NAVI==typeCode){
+			path_start_txt.setEnabled(false);
+//			path_start_txt.setAdapter(arrayAdapter);
+//			path_start_txt.setThreshold(1);
+		}else{
+			path_start_txt.setAdapter(arrayAdapter);
+			path_start_txt.setThreshold(1);
+		}
 		
 		path_end_txt=(AutoCompleteTextView) findViewById(R.id.path_end);
 		path_end_txt.setAdapter(arrayAdapter);
 		path_end_txt.setThreshold(1);
 		
+		
+		
+		btn_return=(Button) findViewById(R.id.path_return);
+		btn_return.setOnClickListener(pathPlanOnClickListener);
+		
+		btn_search=(Button) findViewById(R.id.path_search);
+		btn_search.setOnClickListener(pathPlanOnClickListener);
 		
 		btn_import=(Button) findViewById(R.id.btn_import);
 		btn_import.setOnClickListener(pathPlanOnClickListener);
