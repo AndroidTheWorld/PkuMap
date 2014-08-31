183527769450c713f412c0d805a43d05db17db83
zhangdx <1032425782@qq.com>
2014-08-31 15:32:29 +0800

    增加搜索条目

--- src/com/pkumap/activity/SearchActivity.java
+++ src/com/pkumap/activity/SearchActivity.java
@@ -1,6 +1,9 @@
 package com.pkumap.activity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.pkumap.eventlistener.SearchPoiOnClickListener;
 import com.pkumap.manager.BuildingManager;
@@ -13,10 +16,13 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 public class SearchActivity extends Activity {
 	private AutoCompleteTextView search_edit=null;
 	private Button search_btn=null;
+	private GridView gridView;
 	public PoiManager poiManager;
 	public BuildingManager buildingManager;
 	public String search_map_type="2dmap";
@@ -51,6 +57,25 @@ public class SearchActivity extends Activity {
 		search_btn=(Button) findViewById(R.id.poi_search_btn);
 		search_btn.setOnClickListener(searchPoiOnClickListener);
 		search_edit.setText(str);
+		
+		gridView=(GridView) findViewById(R.id.poi_theme_search);
+		
+		fillGridByThemeLabel();
+		
+	}
+	/**
+	 * 填充GridView
+	 */
+	private void fillGridByThemeLabel(){
+		List<Map<String, Object>> gridItems=new ArrayList<Map<String,Object>>();
+		for(int i=0;i<10;i++){
+			Map<String, Object> gridItem=new HashMap<String,Object>();
+			gridItem.put("poi", "条目"+i);
+			gridItems.add(gridItem);
+		}
+		SimpleAdapter simpleAdapter=new SimpleAdapter(this, gridItems, R.layout.poi_theme_search,
+				new String[]{"poi"},new int[]{R.id.poi_grid_label});
+		gridView.setAdapter(simpleAdapter);
 	}
 	@Override
 	protected void onDestroy() {
