fc1602b17925fa40f7dd543e59c96c490f0b3492
zhangdx <1032425782@qq.com>
2014-09-02 23:24:47 +0800

    将本地的PkuMap.db文件导入到默认数据库路径下

--- src/com/pkumap/db/DatabaseHelper.java
+++ src/com/pkumap/db/DatabaseHelper.java
@@ -1,5 +1,10 @@
 package com.pkumap.db;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,7 +14,7 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
 	private static final int DATABASE_VERSION=1;
-	
+	private static final String DATABASE_PATH="/data/data/com.zdx.pkumap/databases/";
 	private static final String DATABASE_NAME="PkuMap.db";
 	
 	public DatabaseHelper(Context context,String name,CursorFactory factory,
@@ -21,6 +26,36 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	}
 	public DatabaseHelper(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		writeDBFileToDataBase(context);
+	}
+	/**
+	 * 将PkuMap.db写入到默认文件夹下
+	 * @param context
+	 */
+	private void writeDBFileToDataBase(Context context){
+		if ((new File(DATABASE_PATH + DATABASE_NAME)).exists() == false) {
+			File f = new File(DATABASE_PATH);
+			if (!f.exists()) {
+	            f.mkdir();
+	        }
+			try{
+				InputStream is = context.getAssets().open(DATABASE_NAME);
+				OutputStream os = new FileOutputStream(DATABASE_PATH + DATABASE_NAME);
+				
+				byte[] buffer = new byte[1024];
+	            int length;
+	            while ((length = is.read(buffer)) > 0) {
+	                 os.write(buffer, 0, length);
+	            }
+	            
+	            // 关闭文件流
+                os.flush();
+                os.close();
+                is.close();
+			}catch (Exception e) {
+                e.printStackTrace();
+            }
+		}
 	}
 	@Override
 	public void onCreate(SQLiteDatabase db) {
